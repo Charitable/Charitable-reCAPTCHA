9c412f17f3640b30340cf855cb2dab1efb22ac3f
Eric Daams <eric@164a.com>
2019-01-14 10:27:22 +0930

    Initial commit

---
+++ charitable-recaptcha.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Plugin Name:       Charitable - reCAPTCHA
+ * Plugin URI:
+ * Description:
+ * Version:           1.0.0
+ * Author:            WP Charitable
+ * Author URI:        https://www.wpcharitable.com
+ * Requires at least: 4.5
+ * Tested up to:      5.0.3
+ *
+ * Text Domain:       charitable-recaptcha
+ * Domain Path:       /languages/
+ *
+ * @package Charitable/reCAPTCHA/Core
+ * @author  WP Charitable
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
